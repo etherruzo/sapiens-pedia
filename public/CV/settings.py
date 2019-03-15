a0770dfda2d8ee1289e199f7682adab98ec16d4b
etherruzo <etomasherruzo@gmail.com>
2019-03-15 13:07:09 +0100

    Add sapiens-pedia allowed host

--- public/CV/settings.py
+++ public/CV/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'w+p_77&riqh=-i=nr0)nwq)^smk8@2f^p6+jif40who8d=q9$6'
 DEBUG = True
 
 # ALLOWED_HOSTS = ["https://sapiens-1f0c9.firebaseapp.com"]
-ALLOWED_HOSTS = ['ip-172-31-16-112', '127.0.0.1','ec2-18-218-212-136.us-east-2.compute.amazonaws.com',]
+ALLOWED_HOSTS = ['ip-172-31-16-112', '127.0.0.1','ec2-18-218-212-136.us-east-2.compute.amazonaws.com','sapiens-pedia.org',]
 
 # Application definition
 
